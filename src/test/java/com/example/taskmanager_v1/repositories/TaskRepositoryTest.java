d8c9f26fc03ff0b6331b8f031c03026639dedac9
RakshitaMishra03 <51502444+RakshitaMishra03@users.noreply.github.com>
2023-03-19 21:07:17 +0530

    Repository

--- src/test/java/com/example/taskmanager_v1/repositories/TaskRepositoryTest.java
+++ src/test/java/com/example/taskmanager_v1/repositories/TaskRepositoryTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
@@ -18,6 +21,23 @@ public class TaskRepositoryTest {
         task.setDescription("Description 1");
         task.setCompleted(true);
         assertNotNull(taskRepository.save(task));
+
+    }
+
+    @Test
+    public void readTasksWorks(){
+        Task task = new Task();
+        task.setTitle("Task 2");
+        task.setDescription("Description 2");
+        task.setCompleted(true);
+        Task task1 = new Task();
+        task1.setTitle("Task 3");
+        task1.setDescription("Description 3");
+        task1.setCompleted(true);
+        taskRepository.save(task);
+        taskRepository.save(task1);
+        assertNotNull(taskRepository.findAll());
+
 
     }
 }
